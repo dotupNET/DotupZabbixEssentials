5e4261707dfbea95b66fa51aabf92e0898a2dc50
Peter Ullrich <dotup.software@gmail.com>
2020-08-05 14:55:42 +0200

    publishing informations removed

--- publish.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module DotupPsEssentials;
-Import-Module DotupPsModuleGenerator;
-
-$path = Get-Location;
-$PowershellApiKey = "EnterYourPowershellApiKey";
-Publish-PsModule -Path $path -APIKey $PowershellApiKey -IncrementVersion;
